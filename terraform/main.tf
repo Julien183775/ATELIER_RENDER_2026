6f1c4e3d2d0b6e23cf82c2cc3e6970ace005f193
Julien183775 <julien.valadier@2028.icam.fr>
2026-06-17 20:19:45 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+```hcl
 terraform {
   required_providers {
     render = {
@@ -12,27 +13,45 @@ provider "render" {
   owner_id = var.render_owner_id
 }
 
-variable "github_actor" {
-  description = "GitHub username"
-  type        = string
-}
-
 resource "render_web_service" "flask_app" {
   name   = "flask-render-iac-${var.github_actor}"
   plan   = "free"
   region = "frankfurt"
+
   runtime_source = {
     image = {
       image_url = var.image_url
       tag       = var.image_tag
     }
   }
+
   env_vars = {
     ENV = {
       value = "production"
     }
+
     DATABASE_URL = {
-      value = "postgresql://postgres_julien_user:XoLZLFT8fI7VAKOlms1RNKQjyw5NvxU8@dpg-d8pdd4j6sc1c73cl484g-a/postgres_julien"
+      value = var.database_url
     }
   }
 }
+
+resource "render_web_service" "adminer" {
+  name   = "adminer-${var.github_actor}"
+  plan   = "free"
+  region = "frankfurt"
+
+  runtime_source = {
+    image = {
+      image_url = "docker.io/library/adminer"
+      tag       = "latest"
+    }
+  }
+
+  env_vars = {
+    PORT = {
+      value = "8080"
+    }
+  }
+}
+```
