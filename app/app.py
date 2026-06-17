c25883a2e3cd5980e6429c99d7daa520d01b8c16
Julien183775 <julien.valadier@2028.icam.fr>
2026-06-17 19:02:51 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,30 +1,24 @@
-```python
 from flask import Flask
 import os
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
     return "Flask + Docker + GHCR + Terraform + Render"
-
 
 @app.route("/health")
 def health():
     return {"status": "Tout est ok ou pas"}
 
-
 @app.route("/info")
 def info():
     return {
         "app": "Flask Render",
-        "student": "Vala Julien",
+        "student": "Valadier Julien",
         "version": "v1"
     }
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-```
